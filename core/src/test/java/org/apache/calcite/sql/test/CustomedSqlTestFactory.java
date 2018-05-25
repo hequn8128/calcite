9f7324729dc764cd328eea86e89e85c12f36952a
hequn8128 <chenghequn@gmail.com>
2018-05-25 17:18:29 +0800

    [CALCITE-2321] Support ragged fixed length value union be variable

--- core/src/test/java/org/apache/calcite/sql/test/CustomedSqlTestFactory.java
+++ core/src/test/java/org/apache/calcite/sql/test/CustomedSqlTestFactory.java
@@ -16,6 +16,10 @@
  */
 package org.apache.calcite.sql.test;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableMap;
 import org.apache.calcite.adapter.java.JavaTypeFactory;
 import org.apache.calcite.avatica.util.Casing;
 import org.apache.calcite.avatica.util.Quoting;
@@ -34,30 +38,19 @@ import org.apache.calcite.test.CalciteAssert;
 import org.apache.calcite.test.MockCatalogReader;
 import org.apache.calcite.test.MockSqlOperatorTable;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
-
 import javax.annotation.Nonnull;
 
 /**
- * Default implementation of {@link SqlTestFactory}.
- *
- * <p>Suitable for most tests. If you want different behavior, you can extend;
- * if you want a factory with different properties (e.g. SQL conformance level
- * or identifier quoting), wrap in a
- * {@link DelegatingSqlTestFactory} and
- * override {@link #get}.</p>
-*/
-public class DefaultSqlTestFactory implements SqlTestFactory {
+ * Customed implementation of {@link SqlTestFactory}.
+ */
+public class CustomedSqlTestFactory extends DefaultSqlTestFactory {
   public static final ImmutableMap<String, Object> DEFAULT_OPTIONS =
       ImmutableMap.<String, Object>builder()
           .put("quoting", Quoting.DOUBLE_QUOTE)
           .put("quotedCasing", Casing.UNCHANGED)
           .put("unquotedCasing", Casing.TO_UPPER)
           .put("caseSensitive", true)
-          .put("conformance", SqlConformanceEnum.DEFAULT)
+          .put("conformance", SqlConformanceEnum.PRAGMATIC_2003)
           .put("operatorTable", SqlStdOperatorTable.instance())
           .put("connectionFactory",
               CalciteAssert.EMPTY_CONNECTION_FACTORY
@@ -80,83 +73,31 @@ public class DefaultSqlTestFactory implements SqlTestFactory {
                   final SqlOperatorTable operatorTable =
                       factory.createOperatorTable(factory);
                   final JavaTypeFactory typeFactory =
-                      new JavaTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
+                      new JavaTypeFactoryImpl(RelDataTypeSystem.DEFAULT,
+                          (SqlConformance) DEFAULT_OPTIONS.get("conformance"));
                   final MockCatalogReader catalogReader =
                       factory.createCatalogReader(factory, typeFactory);
                   return new Xyz(operatorTable, typeFactory, catalogReader);
                 }
               });
 
-  public static final DefaultSqlTestFactory INSTANCE =
-      new DefaultSqlTestFactory();
+  public static final CustomedSqlTestFactory INSTANCE =
+      new CustomedSqlTestFactory();
 
-  public DefaultSqlTestFactory() {
-  }
-
-  public MockCatalogReader createCatalogReader(SqlTestFactory testFactory,
-      JavaTypeFactory typeFactory) {
-    final boolean caseSensitive = (Boolean) testFactory.get("caseSensitive");
-    return new MockCatalogReader(typeFactory, caseSensitive).init();
-  }
-
-  public SqlOperatorTable createOperatorTable(SqlTestFactory factory) {
-    final SqlOperatorTable opTab0 =
-        (SqlOperatorTable) factory.get("operatorTable");
-    MockSqlOperatorTable opTab = new MockSqlOperatorTable(opTab0);
-    MockSqlOperatorTable.addRamp(opTab);
-    return opTab;
-  }
-
-  public SqlParser createParser(SqlTestFactory factory, String sql) {
-    return SqlParser.create(sql,
-        SqlParser.configBuilder()
-            .setQuoting((Quoting) factory.get("quoting"))
-            .setUnquotedCasing((Casing) factory.get("unquotedCasing"))
-            .setQuotedCasing((Casing) factory.get("quotedCasing"))
-            .setConformance((SqlConformance) factory.get("conformance"))
-            .build());
+  private CustomedSqlTestFactory() {
+    super();
   }
 
   public SqlValidator getValidator(SqlTestFactory factory) {
     final Xyz xyz = cache.getUnchecked(factory);
     final SqlConformance conformance =
         (SqlConformance) factory.get("conformance");
-    return SqlValidatorUtil.newValidator(xyz.operatorTable,
-        xyz.catalogReader, xyz.typeFactory, conformance);
-  }
-
-  public SqlAdvisor createAdvisor(SqlValidatorWithHints validator) {
-    throw new UnsupportedOperationException();
+    return SqlValidatorUtil.newValidator(xyz.getOperatorTable(),
+        xyz.getCatalogReader(), xyz.getTypeFactory(), conformance);
   }
 
   public Object get(String name) {
     return DEFAULT_OPTIONS.get(name);
-  }
-
-  /** State that can be cached and shared among tests. */
-  protected static class Xyz {
-    private final SqlOperatorTable operatorTable;
-    private final JavaTypeFactory typeFactory;
-    private final MockCatalogReader catalogReader;
-
-    Xyz(SqlOperatorTable operatorTable, JavaTypeFactory typeFactory,
-        MockCatalogReader catalogReader) {
-      this.operatorTable = operatorTable;
-      this.typeFactory = typeFactory;
-      this.catalogReader = catalogReader;
-    }
-
-    public SqlOperatorTable getOperatorTable() {
-      return operatorTable;
-    }
-
-    public JavaTypeFactory getTypeFactory() {
-      return typeFactory;
-    }
-
-    public MockCatalogReader getCatalogReader() {
-      return catalogReader;
-    }
   }
 }
 
